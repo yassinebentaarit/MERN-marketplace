0220d7495da16fd7befa7bb32379484faef9b6b8
med yassine bentaarit <mybentaarit@gmail.com>
2023-09-28 01:50:04 +0100

    back and front

---
+++ api/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.listen(4044, () => {
+  console.log('Server is running on port 3000');
+});
+
+
+
+
+// aaslema ness lkol w nchlh mouledkom mabrouk w marhbe bikom ml serie mtaana l nchlh bch twaneskom 3am sne "Smena Lila" fel episode hethi khtarnelkom events yessr mahlehom el conjunction of the moon and jupiter le 2oct w eli houwa yetmathel f i9tiran l 9amar b jupiter w ykounou f a9rab point l baathhom bennesba lina w ykounou wath7in   w peak te3ou bch tkoun bin saetin w 3 taa sbeh w 3anna maah draconid meteor shower w eli bch ysir kima f esmou win l draco constellation (eli talkawha fou9 chwya ml etoile polaire w taht vega hasb sme taana) w bch ykoun hetha le 9 avril w nejmou nchoufou hatta l 20 meteor par heure w haja l wahida l tkhalikch tchoufha hiya l taks w khastn bde yebred taw so elbsou bl behy rodbelkom 3la rwe7km w tfarjou f sme maa smena.aaslema
+// thank you for your support and see you soon <3
