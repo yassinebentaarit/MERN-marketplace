6f9e191489ed4e3bf3e621ae10cd1232cb1e67d2
yassinebentaarit <mybentaarit@gmail.com>
2023-12-13 04:28:11 +0100

    login

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/userRoute.js";
+import authRouter from "./routes/authRoute.js";
 dotenv.config();
 
 mongoose
@@ -13,8 +15,12 @@ mongoose
   });
 
 const app = express();
-app.listen(4044, () => {
-  console.log('Server is running on port 4044');
+
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
 
-app.get
+app.use('/api/user', userRouter);
+app.use('/api/auth',authRouter)
