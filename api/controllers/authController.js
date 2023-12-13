aa3d0ef03cdad22363a1239812f42f876b00a542
yassinebentaarit <mybentaarit@gmail.com>
2023-12-13 04:55:27 +0100

    errors handler

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,15 +1,16 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const {username, email, password} = req.body;
   const hashPassword = bcrypt.hashSync(password,10);
   const newUser = new User({username, email, password:hashPassword});
   try{
     await newUser.save();
+    res.status(201).json('User created !!');
   }
   catch(error){
-    res.status(500).json(error.message);
+    next(error);
   }
-  res.status(201).json('User created !!');
 };
