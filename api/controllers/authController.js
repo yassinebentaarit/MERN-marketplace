b024152fc151b92a90011384dae2b9e9c64b8dbd
yassinebentaarit <mybentaarit@gmail.com>
2023-12-25 10:41:47 +0100

    signIn api

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
@@ -14,3 +15,21 @@ export const signup = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signin = async (req, res, next) => {
+  const {email, password} = req.body;
+  try {
+    const validUser = await User.findOne({email});
+    if(!validUser) return next(errorHandler(404, 'User not found!'));
+    const validPassword = bcrypt.compareSync(password,validUser.password);
+    if(!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+    const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
+    const {password: pass, ...rest} = validUser._doc;
+    res
+      .cookie('access_token', token ,{httpOnly:true , expires: new Date(Date.now()+30*24*60*60*1000)})
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+}
