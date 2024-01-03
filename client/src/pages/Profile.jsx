f7c40ca0b350859c8ba785660a293575254ad514
yassinebentaarit <mybentaarit@gmail.com>
2024-01-03 22:58:25 +0100

    complete delete listing

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -165,6 +165,31 @@ export default function Profile() {
     }
   };
 
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`,{
+        method:'DELETE',
+      })
+      const data =await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return
+      }
+      setUserListing((prev) => prev.filter ((listing) => listing._id !== listingId))
+    } catch (error) {
+     console.log(error.message) 
+    }
+  }
+
+  const handleListingEdit = async (listingId) => {
+    try {
+      
+    } catch (error) {
+     console.log(error.message) 
+    }
+  }
+  
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className=" text-3xl font-semibold text-center my-7">Profile</h1>
@@ -323,10 +348,10 @@ export default function Profile() {
               <p>{listing.name}</p>
             </Link>
             <div className="flex flex-col item-center">
-              <button className="text-red-700 uppercase">
+              <button onClick={()=>handleListingDelete(listing._id)} className="text-red-700 uppercase">
                delete
               </button>
-              <button className="text-green-700 uppercase">
+              <button onClick={()=>handleListingEdit(listing._id)} className="text-green-700 uppercase">
                 edit
               </button>
             </div>
