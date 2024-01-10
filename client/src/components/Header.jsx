c943c08819e8b38d88a771e8d52a1a9d92a8d1df
yassinebentaarit <mybentaarit@gmail.com>
2024-01-10 04:07:12 +0100

    update header search

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Header() {
   const {currentUser } =useSelector(state => state.user)
+  const [searchTerm, setSerchTerm] = useState ('');
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm)
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  }
+  useEffect(()=>{
+    const urlParams = new URLSearchParams(window.location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm')
+    if(searchTermFromUrl) setSerchTerm(searchTermFromUrl)
+  },[location.search])
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -14,13 +28,17 @@ export default function Header() {
             <span className="text-slate-700">ESTLA</span>
           </h1>
         </Link>
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form onSubmit={handleSubmit} className="bg-slate-100 p-3 rounded-lg flex items-center">
           <input
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={(e) => setSerchTerm(e.target.value)}
           />
+          <button>
           <FaSearch className="text-slate-600" />
+          </button>
         </form>
         <ul className="flex gap-4">
         <Link to='/'>
